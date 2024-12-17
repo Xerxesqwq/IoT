2cedc80beb51f2ac8ee44541e6e83fffc5379f70
Zhenxiang Xu <xerxes@icpc.vip>
2024-12-17 22:47:29 +0800

    Analog Devices

--- emulators/Light.py
+++ emulators/Light.py
@@ -8,12 +8,13 @@ class Light:
         # self.user_id = user_id
         # self.pub_topic = f"{self.user_id}/{self.device_id}/pub"
         self.sub_topic = f"devices/control/{self.device_id}"
+        self.pub_topic = f"devices/public/{self.device_id}"
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         self.client.connect(config.MQTT_BROKER, config.MQTT_PORT, 60)
         self.client.subscribe(self.sub_topic)
-        self.client.loop_forever()
+        # self.client.loop_forever()
 
     def on_connect(self, client, userdata, flags, rc):
         print(f"Connected with result code {rc}")
