00c00ab03be2ff61325e588cd46f380948d70924
Zhenxiang Xu <xerxes@icpc.vip>
2024-12-18 17:03:06 +0800

    Integrate DatabaseManager for device lookup in Controller and streamline device management

--- utils/Controller.py
+++ utils/Controller.py
@@ -1,6 +1,7 @@
 from .MQTTController import MQTTController
 from .Devices import Device
 import json
+from .Database import DatabaseManager
 
 class Operation:
     LED_ON = 1
@@ -12,24 +13,18 @@ class Controller:
     def __init__(self):
         self.devices = {}
         self.controller = MQTTController()
-        
-    def add_device(self, device: Device):
-        self.devices[device.device_id] = device
-    
-    def remove_device(self, device_id):
-        del self.devices[device_id]
+        self.db = DatabaseManager(sync_mode=True)
     
     def find_device(self, user_id, device_name):
-        for device in self.devices.values():
-            if device.user_id == user_id and device.name == device_name:
-                return device.device_id
-        return None
+        device_id = self.db.get_device_id(user_id, device_name)
+        return device_id
     
     def device_control(self, user_id, device_name, operation):
         # operation = 1 -> LED ON, 2 -> LED OFF...
         # message: json object
         device_id = self.find_device(user_id, device_name)
-        if device_id is None:
+        if device_id == -1 or device_id is None:
+            print('Device not found')
             return "Device not found"
         message = ""
         if operation == Operation.LED_ON:
